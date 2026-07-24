e3ecc93a035da688bf67904ddfd92602b047db1e
GLKaiky <kaikyfranca8@gmail.com>
2026-07-24 12:20:48 -0300

    readme.md

---
+++ include/barnes_hut/BoundingBox.hpp
@@ -0,0 +1,9 @@
+/**
+ * @file      BoundingBox.hpp
+ * @author    Kaiky França dos Reis Silva
+ * @brief     
+ * @version   0.1
+ * @date      2026-07-24
+ * @copyright Copyright (c) 2026 Kaiky França dos Reis Silva
+ */
+
